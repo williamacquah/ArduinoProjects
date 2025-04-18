b33f5778c1eedfc188e2c17a184105b323c51c32
williamacquah <77838879+williamacquah@users.noreply.github.com>
2025-04-18 23:03:24 +0200

    Add files via upload

---
+++ AnalogWritePWM.ino
@@ -0,0 +1,12 @@
+int redPin = 9;
+int bright = 255;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(redPin, bright);
+}
